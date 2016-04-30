0a07f919f8972a8d17d86044c8107f72e7e1d9ac
dp-huang <yebigroc@gmail.com>
2016-04-30 22:34:25 +0800

    add a basic functionality of react router for navigator

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Link from 'react-router';
+
+import PageRouter from './PageRouter.jsx';
+
+ReactDOM.render(
+	<PageRouter/>
+	, document.getElementById('navigator')
+);
---
+++ src/js/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div>Home page</div>
+		);
+	}
+}
+
+export default Home;
